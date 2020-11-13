7744ee41a7c5ed54601c1bc5112bfb5fc20f4751
celeste1030 <69986268+celeste1030@users.noreply.github.com>
2020-11-13 12:33:24 -0500

    app.py finished

--- app.py
+++ app.py
@@ -29,16 +29,15 @@ def homepage():
         f"<h1>sqlalchemy challenge<h1/>"
         f"<h3>Routes:<h3/>"
         f"Precipitation data for most recent year of data: /api/v1.0/precipitation<br/>"
-        f"Stations: /api/v1.0/stations</<br/>"
+        f"Stations: /api/v1.0/stations<br/>"
         f"Dated temperature observations for most active station for most recent year in data: /api/v1.0/tobs<br/>"
-        f"Temperature normals from the given date: /api/v1.0/yyyy-mm-dd<br/>"
-        f"Temperature normals for the given date range: /api/v1.0/<start> and /api/v1.0/<start>/<end><br/>"
+        f"Temperature normals for the given date range: /api/v1.0/YYYY-MM-DD and /api/v1.0/YYYY-MM-DD/YYYY-MM-DD<br/>"
     )
 
 #Precipitation
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # Create our session (link) from Python to the DB
+    # Create session
     session = Session(engine)
 
     # query for last date in data
@@ -66,13 +65,16 @@ def precipitation():
 
     for date, prcp in prcp_avg:
         prcp_dict = {}
-        prcp_dict[date] = prcp
-        prcp_list.append({f"{date}": number})
+        #prcp_dict[date] = prcp
+        prcp_list.append({f"{date}": prcp})
+
+    session.close()
 
     return (
         jsonify(prcp_list)
     )
-### Return a JSON list of stations from the dataset.
+    
+### Stations
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -86,3 +88,73 @@ def stations():
 
     return jsonify(stations)
 
+#### tobs
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    #create session
+    session = Session(engine)
+
+    # Design a query that lists all stations with their corresponding observation count in descending order.
+
+
+    station_obv = session.query(Measurement.station, func.count(Measurement.tobs)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+
+    print(station_obv)
+
+    # Choose the station with the highest number of temperature observations.
+
+    most_active = "USC00519281"
+
+    print("----------------------------------------")
+    print(f"The most active station is {most_active}.")
+
+    min_date = dt.datetime(2016, 8, 23)
+
+    tobs = session.query(Measurement.tobs).\
+    filter(Measurement.station == most_active).\
+    filter(Measurement.date >= min_date).all()
+
+    session.close()
+
+    total_tobs = []
+
+    for t in tobs:
+        total_tobs.append(t[0])
+    total_tobs
+    
+    return jsonify(total_tobs)
+
+### Start and end date queries
+
+@app.route("/api/v1.0/<start>")
+def first_date(start):
+    session = Session(engine)
+    
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+    session.close()
+
+    tobs_info = list(np.ravel(results))
+
+    return jsonify(tobs_info)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
+
+    tobs_info = list(np.ravel(results))
+
+    return jsonify(tobs_info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
